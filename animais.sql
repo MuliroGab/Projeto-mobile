27961faf0bad7bfb27b73ccdaaefef40dc90a58c
Murillo Gabriel Silva <149397773+MuliroGab@users.noreply.github.com>
2024-09-05 07:17:25 -0300

    Add files via upload

---
+++ animais.sql
@@ -0,0 +1,36 @@
+INSERT INTO Animais (nome_animal, nome_cientifico, continente, som, imagem) VALUES
+-- África
+('Elefante', 'Loxodonta africana', 'África', '/assets/sounds/elefante.mp3', '/assets/images/img_elefante.png'),
+('Leão', 'Panthera leo', 'África', '/assets/sounds/leao.mp3', '/assets/images/img_leao.png'),
+('Girafa', 'Giraffa camelopardalis', 'África', '/assets/sounds/girafa.mp3', '/assets/images/img_girafa.png'),
+('Hipopotamo', 'Hippopotamus amphibius', 'África', '/assets/sounds/hipopotamo.mp3', '/assets/images/img_hipopotamo.png'),
+('Rinoceronte', 'Diceros bicornis', 'África', '/assets/sounds/rinoceronte.mp3', '/assets/images/img_rinoceronte.png'),
+
+-- Ásia
+('Tigre', 'Panthera tigris', 'Ásia', '/assets/sounds/tigre.mp3', '/assets/images/img_tigre.png'),
+('Panda', 'Ailuropoda melanoleuca', 'Ásia', '/assets/sounds/panda.mp3', '/assets/images/img_panda.png'),
+('Elefante Asiático', 'Elephas maximus', 'Ásia', '/assets/sounds/elefante_asiatico.mp3', '/assets/images/img_elefante_asiatico.png'),
+('Dragão de Komodo', 'Varanus komodoensis', 'Ásia', '/assets/sounds/dragao_komodo.mp3', '/assets/images/img_dragao_komodo.png'),
+('Orangotango', 'Pongo pygmaeus', 'Ásia', '/assets/sounds/orangotango.mp3', '/assets/images/img_orangotango.png'),
+
+-- Antártica
+('Pinguim Imperador', 'Aptenodytes forsteri', 'Antártica', '/assets/sounds/pinguim_imperador.mp3', '/assets/images/img_pinguim_imperador.png'),
+('Pinguim Adélia', 'Pygoscelis adeliae', 'Antártica', '/assets/sounds/pinguim_adelia.mp3', '/assets/images/img_pinguim_adelia.png'),
+('Pinguim Chinstrap', 'Pygoscelis antarctica', 'Antártica', '/assets/sounds/pinguim_chinstrap.mp3', '/assets/images/img_pinguim_chinstrap.png'),
+('Pinguim de Barba', 'Pygoscelis antarctica', 'Antártica', '/assets/sounds/pinguim_barba.mp3', '/assets/images/img_pinguim_barba.png'),
+('Pinguim de Galápagos', 'Spheniscus mendiculus', 'Antártica', '/assets/sounds/pinguim_galapagos.mp3', '/assets/images/img_pinguim_galapagos.png'),
+
+-- Oceania
+('Canguru', 'Macropus rufus', 'Oceania', '/assets/sounds/canguru.mp3', '/assets/images/img_canguru.png'),
+('Koala', 'Phascolarctos cinereus', 'Oceania', '/assets/sounds/koala.mp3', '/assets/images/img_koala.png'),
+('Wombat', 'Vombatus ursinus', 'Oceania', '/assets/sounds/wombat.mp3', '/assets/images/img_wombat.png'),
+('Diabo-da-Tasmânia', 'Sarcophilus harrisii', 'Oceania', '/assets/sounds/diabo_tasmania.mp3', '/assets/images/img_diabo_tasmania.png'),
+('Casuar', 'Casuarius casuarius', 'Oceania', '/assets/sounds/casuar.mp3', '/assets/images/img_casuar.png'),
+
+-- América do Norte
+('Águia Careca', 'Haliaeetus leucocephalus', 'América do Norte', '/assets/sounds/aguia_careca.mp3', '/assets/images/img_aguia_careca.png'),
+('Urso Grizzly', 'Ursus arctos horribilis', 'América do Norte', '/assets/sounds/urso_grizzly.mp3', '/assets/images/img_urso_grizzly.png'),
+('Bisonte', 'Bison bison', 'América do Norte', '/assets/sounds/bisonte.mp3', '/assets/images/img_bisonte.png'),
+('Puma', 'Puma concolor', 'América do Norte', '/assets/sounds/puma.mp3', '/assets/images/img_puma.png'),
+('Lobo Cinzento', 'Canis lupus', 'América do Norte', '/assets/sounds/lobo_cinzento.mp3', '/assets/images/img_lobo_cinzento.png');
+
